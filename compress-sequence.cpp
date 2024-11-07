215d2be2f015d870c9313d4b0dc5cf43c2041bb7
ilyas-9 <84283144+ilyas-9@users.noreply.github.com>
2024-11-07 21:29:50 +0100

    Update compress-sequence.cpp
    just adding some comments for more understanding

--- compress-sequence.cpp
+++ compress-sequence.cpp
@@ -3,12 +3,14 @@
 using namespace std;
 
 int main(){
-    string str , s = "" , num = "";
-    getline(cin,str);
-    int n = 0;
+    // create 3 string that we will be using during the programe .
+    string str , s = "" , num = ""; //str is the string that we will be compressing , s is the returnd string .
+                                    // num will be storing the number after converting it into a string .
+    getline(cin,str);        // its for get the sequence from the user .
+    int n = 0;               // storing number of how many times the character is repeated .
 
     for ( int j, i = 0; i < (int)str.length() ; ){
-        if ( str[i] == ' '){
+        if ( str[i] == ' '){        // this part is handling the case of the space .
             s.insert(s.end(),' ');
             i++;
             continue;
@@ -16,17 +18,17 @@ int main(){
             n = 1;
         num.clear();
         for (  j = i+1 ; j <= (int)str.length() ; j++){
-            if ( str[i] == str[j] && j<(int)str.length() ){
+            if ( str[i] == str[j] && j<(int)str.length() ){    // it just keep counting if it still the same character .
                 n++;
-            }else{
+            }else{ // here whene the inserting begin .
                 s.insert(s.end(),str[i]);
                 s.insert(s.end(),'*');
-                while ( n > 0 ){
+                while ( n > 0 ){ // this part converting the number into a string .
                     num.insert(num.begin(),n%10+48);
                     n/=10;
                 }
-                for ( int k : num){
-                    s.insert(s.end(), k);
+                for ( int k : num){        // this loop is for inserting the string, i know insert() function can do that, 
+                    s.insert(s.end(), k);  // but it didn't in my code::blocks and i use it any way
                 }
                 s.insert(s.end(),'.');
                 i=j;
@@ -36,3 +38,4 @@ int main(){
     }
     cout << s << endl;
 }
+// I think there is a way to improve this, but important thing, it works.
